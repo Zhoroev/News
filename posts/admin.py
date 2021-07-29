3b29048965a152eb7a127ea78e8ffd467d30cb73
user <erjan.joroev.kg@gmail.com>
2021-07-29 20:56:12 +0600

    Set up admin site

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    field = ['title', 'body']
+
+
+admin.site.register(Post, PostAdmin)
